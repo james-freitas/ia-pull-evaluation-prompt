6b331612cfd3f67ccf3bed73eb6780a0074303e2
James Freitas <jamesfrj@yahoo.com.br>
2026-03-02 22:16:27 -0300

    feat: Add feature to pull prompts from langsmith and save them in a file

--- src/pull_prompts.py
+++ src/pull_prompts.py
@@ -13,20 +13,31 @@ import os
 import sys
 from pathlib import Path
 from dotenv import load_dotenv
-from langchain import hub
+from langsmith import Client
+from langchain_openai import ChatOpenAI
+#from langchain import hub
+from langchain.load.dump import dumps
 from utils import save_yaml, check_env_vars, print_section_header
 
 load_dotenv()
 
-
 def pull_prompts_from_langsmith():
-    ...
+    client = Client()
+    prompt = client.pull_prompt("leonanluppi/bug_to_user_story_v1")
 
+    prompt_data = dumps(prompt)
+
+    save_path = Path("prompts/raw_prompts.yml")
+    save_path.parent.mkdir(parents=True, exist_ok=True)
+
+    with open(save_path, "w", encoding="utf-8") as f:
+        f.write(prompt_data)
+
+    print(f"✅ Prompt salvo com sucesso em {save_path}")    
 
 def main():
     """Função principal"""
-    ...
-
+    pull_prompts_from_langsmith()
 
 if __name__ == "__main__":
     sys.exit(main())
