0a49b1d8353c842b0ca0a412bdbc20ab3c324ca5
James Freitas <jamesfrj@yahoo.com.br>
2026-04-01 22:04:27 -0300

    update evaluate script

--- src/evaluate.py
+++ src/evaluate.py
@@ -312,7 +312,7 @@ def main():
 
     prompts_to_evaluate = [
         #"bug_to_user_story_v2",
-        "bug-to-user-story-using-chain-of-thought/bug_to_user_story_using_fewshot"
+        "awjamesfrj/bug_to_user_story_using_fewshot"
     ]
 
     all_passed = True
